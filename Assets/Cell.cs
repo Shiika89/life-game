5a3d99a72fb1fc4f7fbf9576b6ad8131831022b3
shiika89 <kazuki_plant@yahoo.co.jp>
2021-06-07 20:26:09 +0900

    セルを生成して並べる、周囲を調べて生死を決める内容を修正中

---
+++ Assets/Cell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Cell : MonoBehaviour
+{
+    public bool m_living = false;
+    Image m_color;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_color = GetComponent<Image>();
+        //m_living = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LivingColor();
+    }
+
+    public void LivingColor()
+    {
+        if (m_living == false)
+        {
+            m_color.color = Color.black;
+        }
+        if (m_living == true)
+        {
+            m_color.color = Color.green;
+        }
+    }
+}
---
+++ Assets/LifeGame.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeGame : MonoBehaviour
+{
+    [SerializeField] Cell m_cellPrefab;
+    [SerializeField] GridLayoutGroup m_gridLayoutGroup;
+    [SerializeField] int m_numX = 0;
+    [SerializeField] int m_numY = 0;
+    [SerializeField] int m_initial = 0;
+    Cell[,] m_cubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cellを綺麗に並べる
+        if (m_numX < m_numY)
+        {
+            m_gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            m_gridLayoutGroup.constraintCount = m_numX;
+        }
+        else
+        {
+            m_gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedRowCount;
+            m_gridLayoutGroup.constraintCount = m_numY;
+        }
+
+        m_cubes = new Cell[m_numX, m_numY];
+
+        //セルを生成する
+        for (int i = 0; i < m_numX; i++)
+        {
+            for (int x = 0; x < m_numY; x++)
+            {
+                var cell = Instantiate(m_cellPrefab);
+                var parent = m_gridLayoutGroup.gameObject.transform;
+                cell.transform.SetParent(parent);
+                m_cubes[i, x] = cell;
+            }
+        }
+
+        //最初にランダムな位置に生きてるセルを生成する
+        if (m_initial < m_numX * m_numY)
+        {
+            for (int i = 0; i < m_initial; i++)
+            {
+                var x = Random.Range(0, m_numX);
+                var y = Random.Range(0, m_numY);
+                var cell = m_cubes[x, y];
+
+                if (cell.m_living == false)
+                {
+                    cell.m_living = true;
+
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+    }
+
+    public Cell[] SearchCell(int r, int c)
+    {
+        var list = new List<Cell>();
+
+        var left = c - 1;
+        var right = c + 1;
+        var top = r - 1;
+        var bottom = r + 1;
+
+        if (top >= 0)
+        {
+            if (left >= 0)
+            {
+                list.Add(m_cubes[top, left]);
+            }
+            list.Add(m_cubes[top, c]);
+            if (right < m_numX)
+            {
+                list.Add(m_cubes[top, right]);
+            }
+        }
+        if (left >= 0)
+        {
+            list.Add(m_cubes[r, left]);
+        }
+        if (right < m_numX)
+        {
+            list.Add(m_cubes[r, right]);
+        }
+        if (bottom < m_numY)
+        {
+            if (left >= 0)
+            {
+                list.Add(m_cubes[bottom, left]);
+            }
+            list.Add(m_cubes[bottom, c]);
+            if (right < m_numX)
+            {
+                list.Add(m_cubes[bottom, right]);
+            }
+        }
+
+        return list.ToArray();
+    }
+}
